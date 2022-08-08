7573b69d010bc13ab081e88a48b98ad52edadb9d
dperformance <docyoungs@naver.com>
2022-08-08 10:24:57 +0900

    Feat: add Task class, implement GET method
    Task class를 추가함.
    
    GET method 1차 구현
     * getTasks
       - http://localhost:8000/tasks
     * getTask
       - http://localhost:8000/tasks/{id}

--- app/src/main/java/com/restapi/study/TaskHttpHandler.java
+++ app/src/main/java/com/restapi/study/TaskHttpHandler.java
@@ -1,5 +1,6 @@
 package com.restapi.study;
 
+import com.restapi.study.models.Task;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -9,11 +10,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TaskHttpHandler implements HttpHandler {
+    private final Map<Long, String> tasks = new HashMap<>();
 
+    public TaskHttpHandler() {
 
+        tasks.put(1L, "Dyson");
+        tasks.put(2L, "qwer");
+        tasks.put(3L, "asdf");
+        tasks.put(4L, "asdcv");
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -32,23 +42,33 @@ public class TaskHttpHandler implements HttpHandler {
         System.out.println(body);
 
         // TODO
-        // http://localhost:8888 Create a response message
-        //   - output stream
-        // implement Get method
+        // implement GET Method
+        //  - /tasks (getTasks)
+        //  - /tasks/{id} (getTask)
+        // implement POST Method
+        //  - 조회 하기 위해 POST도 구현을 해야 한다.
 
-        
+        if (method.equals("GET")) {
+            if (path.equals("/tasks")) {
+                System.out.println("get all");
+                System.out.println(tasks);
+            }
+            if (path.startsWith("/tasks/")) {
+                Long id = Long.parseLong(path.substring("/tasks/".length()));
+                System.out.println(tasks.get(id));
+            }
+        }
+
+
+
+    }
+    // response format
+    private void responseSend(String content, HttpExchange exchange) throws IOException {
         exchange.sendResponseHeaders(200, content.getBytes(StandardCharsets.UTF_8).length);
 
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(content.getBytes(StandardCharsets.UTF_8));
         outputStream.flush();
         outputStream.close();
-
-
-
-//        if (method.equals("GET") && path.equals("/tasks")) {
-//            System.out.println("GET Check");
-//        }
-
     }
 }
