b7fdbdcbd15554218f4a8a00d35e9fc506878241
dperformance <docyoungs@naver.com>
2022-09-28 21:00:55 +0900

    Build: Add Lombok

---
+++ app/src/main/java/com/restapi/study/domain/Product.java
@@ -0,0 +1,23 @@
+// Product Model
+// User Model
+// Order Model
+// ... Model
+// Application (UseCase)
+// Product ->  관리자 등록/수정/삭제 -> List/detail/
+// 사용자 주문 -> 확인 -> 배송 등 처리
+
+// Product
+// 0. 식별자 - identifier (ID)
+// 1. 제품 이름 - 쥐돌이 장난감
+// 2. 제조사 - 덕구월드
+// 3. 가격 - 5,000(판매가)
+// 4. 이미지 - static, CDN => image URL
+package com.restapi.study.domain;
+
+public class Product {
+
+
+    public String getName() {
+        return "";
+    }
+}
---
+++ app/src/test/java/com/restapi/study/domain/ProductTest.java
@@ -0,0 +1,17 @@
+package com.restapi.study.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductTest {
+
+
+    @Test
+    void creation() {
+        Product product = new Product();
+        assertThat(product.getName()).isEmpty();
+
+    }
+
+}
