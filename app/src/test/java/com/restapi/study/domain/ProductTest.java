e962f0044fecfde53d5786bee831f9fc3d21c37d
dperformance <docyoungs@naver.com>
2023-01-04 07:39:23 +0900

    Test: Product class Test Add

--- app/src/test/java/com/restapi/study/domain/ProductTest.java
+++ app/src/test/java/com/restapi/study/domain/ProductTest.java
@@ -1,17 +1,32 @@
+// Product
+// 0. 식별자(id) - identifier
+// 1. 제품 이름(name) - 아우터
+// 2. 제조사(maker) - nike
+// 3. 가격(price) - 1,000,000
+// 4. 이미지(image) - static, CDN => image URL
+
 package com.restapi.study.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductTest {
 
-
     @Test
-    void creation() {
-        Product product = new Product();
-        assertThat(product.getName()).isEmpty();
+    void creationWithBuilder() {
+        Product product = Product.builder()
+                .id(1L)
+                .name("아우터")
+                .maker("nike")
+                .price(5000)
+                .build();
 
+        assertThat(product.getId()).isEqualTo(1L);
+        assertThat(product.getName()).isEqualTo("아우터");
+        assertThat(product.getMaker()).isEqualTo("nike");
+        assertThat(product.getPrice()).isEqualTo(5000);
+        assertThat(product.getImageUrl()).isNull();
     }
-
 }
